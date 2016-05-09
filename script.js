94cbeb7d8dbad0a1cca6b97c6d5f4c82f029c1ac
Mikhail <chesnokov_ma@mail.ru>
2016-05-10 04:27:02 +0500

    ДЗ четвёртого урока

--- script.js
+++ script.js
@@ -1,48 +1,37 @@
 window.onload = function () {
-
-// 1. Найти все элементы h1, являющиеся потомками div и заменить их содержимое на произвольное.
-    $('div.task div > h1').text('Заменил все заголовоки h1')
-
-// 2. Установить всем input type="text" произвольное содержимое. Содержимое второго инпута должно отличаться от первого.
-    // $('div.task input[type="text"]').eq(0).attr('value','Установил значение Первого инпута')
-    // $('div.task input[type="text"]').eq(1).attr('value','Установил значение Второго инпута')
-    $('div.task input[type="text"]').eq(0).val('Установил значение Первого инпута')
-    $('div.task input[type="text"]').eq(1).val('Установил значение Второго инпута')
-    // А можно ещё таким образом
-    // $('div.task input[type="text"]:first').attr('value','Установил значение Первого инпута')
-    // $('div.task input[type="text"]:first').next().next().attr('value','Установил значение Второго инпута')
-
-// 3. Выведите значение option с value=2
-    var textOption_val2 = $('#myselect option[value="2"]').text()
-    // Выбираем в списке значение option с value=2
-    // $('#myselect option[value="2"]').attr('selected','selected')
-    $('#myselect option[value="2"]').prop('selected',true)
-    // Получаем и выводим значение option с value=2
-    textOption_val2 = '<p>' + textOption_val2 + '</p>'
-    $('#myselect').after(textOption_val2)
-    $('#myselect').after('<p>Выбрали и вывели значение option с value=2:</p>')
-    $('#myselect').nextAll().addClass('red')
-    $('#myselect').nextAll().eq(1).addClass('bold left_padding_20')
-
-// 4. Обернуть текст во втором li тегом b:
-    $('div.task ul').children().eq(1).wrap('<b></b>')
-    //  А можно ещё таким образом
-//    textLi2 = '<b>' + $('div.task ul').children().eq(1).text() + '</b>'
-//    $('div.task ul').children().eq(1).html(textLi2)
-
-// 5. Извлеките последний li из предыдущего примера.
-    //    Сохраняем элемент DOM в переменной
-    var elementLast_li = $('div.task ul > li:last-child').clone()
-    // Получаем текст ранее найденного элемента
-    var textLast_li = elementLast_li.text()
-    //  А можно ещё таким образом
-    //    $('div.task ul > li:last-child').text()
-    //    $('div.task ul > li').filter(':last').text()
-
-    var pText = document.createElement('p')
-    pText.innerText = textLast_li
-    $('div.task ul').after(pText)
-    $('div.task ul').after('<p>Текст последнего li:</p>')
-    $('div.task ul').nextAll().addClass('red')
-    $('div.task ul').next().next().addClass('bold left_padding_20')
+    (function ($) {
+        console.log('Привет!')
+        $('.context_menu > .context_active').show();
+        $('.menu_label').on('click','li',function () {
+//            console.log( $( this ).text());
+            if (!$(this).hasClass('label_active')) {
+                $('.menu_label > .label_active').addClass('label_passive');
+                $('.menu_label > .label_active').removeClass('label_active');
+                $(this).removeClass('label_passive');
+                $(this).addClass('label_active');
+                var index = $('.menu_label li').index(this);
+//                console.log(index);
+                $('.context_menu > .context_active').hide();
+//                $('.context_menu > .context_active').fadeOut(800,function () {
+//                    $('.context_menu > .context_active').removeClass('context_active');
+//                    $('.context_menu div').eq(index).fadeIn(800,function () {
+//                    });
+ //                   $('.context_menu div').eq(index).addClass('context_active');
+ //               });
+                $('.context_menu > .context_active').removeClass('context_active');
+                $('.context_menu div').eq(index).addClass('context_active');
+                $('.context_menu div').eq(index).show();
+            }
+        });
+//        $('.menu_label').on('mouseover','li', function () {
+//            if ($(this).hasClass('label_passive')) {
+//                $(this).addClass('label_focus');
+//            };
+//        });
+//        $('.menu_label').on('mouseout','li', function () {
+//            if ($(this).hasClass('label_focus')) {
+//                $(this).removeClass('label_focus');
+//            };
+//        });
+    })(jQuery);
 }
