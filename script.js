799a742491bfc0c3d182b31d39f57413a904331e
Mikhail <chesnokov_ma@mail.ru>
2016-05-18 02:28:31 +0500

    ДЗ урока №6

--- script.js
+++ script.js
@@ -1,37 +1,27 @@
 window.onload = function () {
     (function ($) {
-        console.log('Привет!')
-        $('.context_menu > .context_active').show();
-        $('.menu_label').on('click','li',function () {
-//            console.log( $( this ).text());
-            if (!$(this).hasClass('label_active')) {
-                $('.menu_label > .label_active').addClass('label_passive');
-                $('.menu_label > .label_active').removeClass('label_active');
-                $(this).removeClass('label_passive');
-                $(this).addClass('label_active');
-                var index = $('.menu_label li').index(this);
-//                console.log(index);
-                $('.context_menu > .context_active').hide();
-//                $('.context_menu > .context_active').fadeOut(800,function () {
-//                    $('.context_menu > .context_active').removeClass('context_active');
-//                    $('.context_menu div').eq(index).fadeIn(800,function () {
-//                    });
- //                   $('.context_menu div').eq(index).addClass('context_active');
- //               });
-                $('.context_menu > .context_active').removeClass('context_active');
-                $('.context_menu div').eq(index).addClass('context_active');
-                $('.context_menu div').eq(index).show();
-            }
+        $('.cont_forma').on('click','#reg_button',function (event) {
+            console.log('Click');
+            var success = function (data) {
+                console.log(data);
+                $(":input").removeClass('error_input');
+                $('div.cont_response').empty();
+                $('div.cont_response').next().remove();
+                if ($.trim(data.result) == 'true') {
+                    $('div.cont_response').html('Ваши данные прошли проверку');
+                } else {
+                    var $ol = $('<ol>');
+                    $('div.cont_response').html('Ошибки заполнения формы:');
+                    $.each(data.error, function (key, error_name) {
+                        var name_input = key.trim().replace(' ','_').toLowerCase();
+                        $(":input[name=" + name_input + "]").addClass('error_input');
+                        $ol.append($('<li>').text(error_name));
+                    });
+                    $('div.cont_response').after($ol);
+                };
+            };
+            $.post('validator.php',$("#form_reg").serializeArray(),success,'json');
+            event.preventDefault();
         });
-//        $('.menu_label').on('mouseover','li', function () {
-//            if ($(this).hasClass('label_passive')) {
-//                $(this).addClass('label_focus');
-//            };
-//        });
-//        $('.menu_label').on('mouseout','li', function () {
-//            if ($(this).hasClass('label_focus')) {
-//                $(this).removeClass('label_focus');
-//            };
-//        });
     })(jQuery);
 }
