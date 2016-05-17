2bd8b29bd2e8ca7517c4bbb6de5cdabc18ab38f5
Mikhail <chesnokov_ma@mail.ru>
2016-05-18 02:27:46 +0500

    ДЗ урока №5

--- script.js
+++ script.js
@@ -1,37 +1,16 @@
 window.onload = function () {
     (function ($) {
-        console.log('Привет!')
-        $('.context_menu > .context_active').show();
-        $('.menu_label').on('click','li',function () {
-//            console.log( $( this ).text());
-            if (!$(this).hasClass('label_active')) {
-                $('.menu_label > .label_active').addClass('label_passive');
-                $('.menu_label > .label_active').removeClass('label_active');
-                $(this).removeClass('label_passive');
-                $(this).addClass('label_active');
-                var index = $('.menu_label li').index(this);
-//                console.log(index);
-                $('.context_menu > .context_active').hide();
-//                $('.context_menu > .context_active').fadeOut(800,function () {
-//                    $('.context_menu > .context_active').removeClass('context_active');
-//                    $('.context_menu div').eq(index).fadeIn(800,function () {
-//                    });
- //                   $('.context_menu div').eq(index).addClass('context_active');
- //               });
-                $('.context_menu > .context_active').removeClass('context_active');
-                $('.context_menu div').eq(index).addClass('context_active');
-                $('.context_menu div').eq(index).show();
-            }
+        $('.cont_forma').on('click','#reg_button',function (event) {
+            console.log('Click');
+            var success = function (data) {
+                if ($.trim(data) == 'true') {
+                    $('div.cont_response').html('Ваши данные прошли проверку');
+                } else {
+                    $('div.cont_response').html('Ваши данные не прошли проверку');
+                };
+            };
+            $.post('validator.php',$("#form_reg").serializeArray(),success);
+            event.preventDefault();
         });
-//        $('.menu_label').on('mouseover','li', function () {
-//            if ($(this).hasClass('label_passive')) {
-//                $(this).addClass('label_focus');
-//            };
-//        });
-//        $('.menu_label').on('mouseout','li', function () {
-//            if ($(this).hasClass('label_focus')) {
-//                $(this).removeClass('label_focus');
-//            };
-//        });
     })(jQuery);
 }
